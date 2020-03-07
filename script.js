3563ea9677261e7c1da57f19a345c61593da9f2e
fox-yokai <pat.w.cain@gmail.com>
2020-03-07 15:46:28 -0600

    added feature to hide start block and display quiz block

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-var quizArr = ""
+var quizArr = {
+    questionOne: {
+        question: "This is a sample questions 1?",
+        answerOne: "This is the first answer (1).",
+        answerTwo: "This is the second answer (1).",
+        answerThree: "This is the third answer (1).",
+        answerFour: "This is the fourth answer (1).",
+    }
+    questionTwo: {
+        question: "This is a sample questions 1?",
+        answerOne: "This is the first answer (2).",
+        answerTwo: "This is the second answer (2).",
+        answerThree: "This is the third answer (2).",
+        answerFour: "This is the fourth answer (2).",
+    }
+    questionThree: {
+        question: "This is a sample questions 1?",
+        answerOne: "This is the first answer (3).",
+        answerTwo: "This is the second answer (3).",
+        answerThree: "This is the third answer (3).",
+        answerFour: "This is the fourth answer (3).",
+    }
+};
+
+function quizQuestions() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("quizWindow").style.display = "block";
+}
+
+function quizTimeLeft() {
+    
+}
