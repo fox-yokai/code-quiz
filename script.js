fa0033c5289778c88069694f05fc3f849c907062
fox-yokai <pat.w.cain@gmail.com>
2020-03-09 09:45:03 -0500

    quiz questions will display

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var quizArr = [
-    ["What is a question one?", "This is sample answer 1A", "This is sample answer 1B", "This is sample answer 1C", "This is sample answer 1D", "D"],
-    ["What is a question two?", "This is sample answer 2A", "This is sample answer 2B", "This is sample answer 2C", "This is sample answer 2D", "A"],
-    ["What is a question three?", "This is sample answer 3A", "This is sample answer 3B", "This is sample answer 3C", "This is sample answer 3D", "B"],
+    ["What is a question one?", "This is sample answer 1A", "This is sample answer 1B", "This is sample answer 1C", "This is sample answer 1D", "This is sample answer 1A"],
+    ["What is a question two?", "This is sample answer 2A", "This is sample answer 2B", "This is sample answer 2C", "This is sample answer 2D", "This is sample answer 2C"],
+    ["What is a question three?", "This is sample answer 3A", "This is sample answer 3B", "This is sample answer 3C", "This is sample answer 3D", "This is sample answer 3D"],
 ];
 
 var timeLeft = 99;
-var i = 0
-var qLoop = 0
+var i = 0;
+var qLoop = 0;
+var score = 0;
 document.getElementById("timer").innerHTML = timeLeft;
 
 var qAnswers = document.getElementById("qChoices");
@@ -33,10 +34,22 @@ function showAnswers() {
 }
 
 qAnswers.addEventListener("click", function(event){
-    if (event.target.innerHTML === quizArr[i][qLoop][5]) {
-        console.log("success")
+    if (event.target.innerHTML === quizArr[i][5]) {
+        guessCorrect();
+    } else {
+        guessWrong();
     }
 })
+
+function guessCorrect() {
+    score = score + 5;
+    qLoop++;
+    quizStart();
+}
+
+function guessWrong() {
+    console.log("incorrect")
+}
 
 
 function quizTimeLeft() {
