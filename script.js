e7367eba76cb4d4d5bf9b021dbc0c511e020dfac
fox-yokai <pat.w.cain@gmail.com>
2020-03-08 19:00:29 -0500

    Quiz displays question and choices

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
-var quizArr = {
-    questionOne: {
-        question: "This is a sample questions 1?",
-        answerOne: "This is the first answer (1).",
-        answerTwo: "This is the second answer (1).",
-        answerThree: "This is the third answer (1).",
-        answerFour: "This is the fourth answer (1).",
-    },
-    questionTwo: {
-        question: "This is a sample questions 1?",
-        answerOne: "This is the first answer (2).",
-        answerTwo: "This is the second answer (2).",
-        answerThree: "This is the third answer (2).",
-        answerFour: "This is the fourth answer (2).",
-    },
-    questionThree: {
-        question: "This is a sample questions 1?",
-        answerOne: "This is the first answer (3).",
-        answerTwo: "This is the second answer (3).",
-        answerThree: "This is the third answer (3).",
-        answerFour: "This is the fourth answer (3).",
-    }
-};
+var quizArr = [
+    ["What is a question one?", "This is sample answer 1A", "This is sample answer 1B", "This is sample answer 1C", "This is sample answer 1D", "D"],
+    ["What is a question two?", "This is sample answer 2A", "This is sample answer 2B", "This is sample answer 2C", "This is sample answer 2D", "A"],
+    ["What is a question three?", "This is sample answer 3A", "This is sample answer 3B", "This is sample answer 3C", "This is sample answer 3D", "B"],
+];
 
-var timeLeft = 10;
+var timeLeft = 99;
+var i = 0
+var qLoop = 0
 document.getElementById("timer").innerHTML = timeLeft;
+
+var qAnswers = document.getElementById("qChoices");
 
 function quizQuestions() {
     document.getElementById("start").style.display = "none";
     document.getElementById("quizWindow").style.display = "block";
-    quizTimeLeft()
+    quizTimeLeft();
+    quizStart();
 }
+
+function quizStart() {
+    document.getElementById("winQuestion").textContent = quizArr[i][0];
+    showAnswers();
+}
+
+function showAnswers() {
+    for (var q = 1; q <= 4; q++) {
+        var qChoice = quizArr[i][q];
+        var li = document.createElement("li");
+        li.textContent = qChoice;
+        qAnswers.appendChild(li);  
+        }
+}
+
+qAnswers.addEventListener("click", function(event){
+    if (event.target.innerHTML === quizArr[i][qLoop][5]) {
+        console.log("success")
+    }
+})
+
 
 function quizTimeLeft() {
     var timerInerval = setInterval(function () {
