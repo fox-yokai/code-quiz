d8ed59d62990898fab1e0cfd2bf7f870526e3f9e
fox-yokai <pat.w.cain@gmail.com>
2020-03-10 21:09:42 -0500

    scores are written to localStorage and are displayed

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 var quizArr = [
     {
-     question: "This is the first sample question?",
-     choices: ["This is the correct answer for 1", "This is the wrong answer for 1", "This is the wrong answer for 1", "This is the wrong answer for 1"],
-     correctAnswer: "This is the correct answer for 1", 
+     question: "What is NOT a feature of JavaScript",
+     choices: ["JavaScript is not designed to creating network-centric applications", "JavaScript is a lightweight, interpreted programming language.", "JavaScript is complementary to and integrated with Java.", "JavaScript is open and cross-platform."],
+     correctAnswer: "JavaScript is not designed to creating network-centric applications", 
     },
     {
-        question: "This is the second sample question?",
-        choices: ["This is the wrong answer for 1", "This is the correct answer for 2", "This is the wrong answer for 1", "This is the wrong answer for 1"],
-        correctAnswer: "This is the correct answer for 2", 
+        question: "What are the advantages of JavaScript?",
+        choices: ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above"],
+        correctAnswer: "All of the above", 
     },
     {
-        question: "This is the third sample question?",
-        choices: ["This is the wrong answer for 1", "This is the wrong answer for 1","This is the correct answer for 3", "This is the wrong answer for 1"],
-        correctAnswer: "This is the correct answer for 3", 
+        question: "How to add one new element at the end of an array?",
+        choices: ["pull()", "push()","splice()", "pop()"],
+        correctAnswer: "push()", 
+    },
+    {
+       question: "How do you declare a variable?",
+       choices: ["console.log())", "if","function", "var"],
+       correctAnswer: "var", 
+      },
+      {
+        question: "What is the correct syntax for referring to an external script called 'script.js'",
+        choices: ["<script src='script.js'>", "<script href='script.js'>","<script ref='script.js>'", "script name='script.js'"],
+        correctAnswer: "<script src='script.js'>", 
+       },
+       {
+        question: "Which of the following is not a reserved word in JavaScript?",
+        choices: ["interface", "throws","program", "short"],
+        correctAnswer: "program", 
+       },
+       {
+        question: "Which of the following is the correct syntax to display “Warning!” in an alert box using JavaScript?",
+        choices: ["alertbox('Warning!');", "msg('Warning!');","msgbox('Warning!');", "alert('Warning!);"],
+        correctAnswer: "alert('Warning!);", 
+       },
+       {
+        question: " What is the HTML tag under which one can write the JavaScript code?",
+        choices: ["<javascript>", "<scripted>","<script>", "<js>"],
+        correctAnswer: "<script>", 
        }
 ];
 
@@ -23,6 +48,7 @@ document.getElementById("timer").innerHTML = timeLeft;
 
 var qAnswers = document.getElementById("qChoices");
 var submitInitials = document.getElementById("submitInitials");
+var resetScores = document.getElementById("reset-scores");
 
 function quizQuestions() {
     document.getElementById("start").style.display = "none";
@@ -32,7 +58,7 @@ function quizQuestions() {
 }
 
 function quizStart() {
-    if (i > 2) {
+    if (i > 7) {
         gameOver();
     } else {
         document.getElementById("qChoices").textContent = "";
@@ -79,9 +105,32 @@ function gameOver() {
 }
 
 submitInitials.addEventListener("click", function(event) {
+    var scoresEl = JSON.parse(localStorage.getItem("game-score"));
     var initials = document.getElementById("initials").value;
-    localStorage.setItem("game-score", initials + " " + score);
+    var intScore = initials + " " +score;
+    // cannot push to am array of null
+    scoresEl = scoresEl || [];
+    scoresEl.push(intScore);
+    localStorage.setItem("game-score", JSON.stringify(scoresEl))
     document.location.href = "https://fox-yokai.github.io/code-quiz/scores.html";
-} )
+    // document.location.href = "file:///C:/Users/kitsu/Documents/CodingBootCamp/uofm-stp-fsf-ft-02-2020-u-c/homework/code-quiz/scores.html";
+}
+)
+
+function displayScores() {
+    highScore = JSON.parse(localStorage.getItem("game-score"));
+    for (var i = 0; i < highScore.length; i++) {
+        var li2 = document.createElement("li"); 
+
+        console.log(highScore[i])
+        li2.textContent = highScore[i];
+        document.getElementById("high-scores").appendChild(li2);
+    }
 
 
+}
+
+resetScores.addEventListener("click", function(event) {
+    localStorage.removeItem("game-score");
+    document.getElementById("high-scores").textContent = "";
+})
